f493ac90dd9caecda74ff5136923149847427a5d
Josh Creter <jcreter@gmail.com>
2011-05-23 22:36:37 -0700

    Starting to factor out channel info

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,4 +1,6 @@
 class BrainsController < ApplicationController
+
+  @@channel_id = "2819176B-EEBE-4C70-9BFE-1CCA04A31267"
   def start
     REDIS.set("keep_alive", "true")
     redirect_to :back
@@ -10,13 +12,13 @@ class BrainsController < ApplicationController
   end
 
   def loopy
-    brain = BrainJob.new("loopy", "F900A25F-5C39-42FF-8CBD-AA7E80F8F94E")
+    brain = BrainJob.new("loopy", @@channel_id)
     Delayed::Job.enqueue brain
     redirect_to :back
   end
 
   def onetime
-    brain = BrainJob.new("birdbrain", "2819176B-EEBE-4C70-9BFE-1CCA04A31267")
+    brain = BrainJob.new("birdbrain", @@channel_id)
     brain.perform
     redirect_to :back
   end
