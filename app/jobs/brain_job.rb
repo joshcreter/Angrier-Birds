de45baf570614bae5527ae1c37ff530633e7e8ce
Josh Creter <jcreter@gmail.com>
2011-05-23 20:48:18 -0700

    Updating channel ID

--- app/jobs/brain_job.rb
+++ app/jobs/brain_job.rb
@@ -41,7 +41,7 @@ class BrainJob < Struct.new(:channel_id, :brain_id)
     while loopcount > 0
       message_body = "#{Time.now} - [wit goes in here] - Countdown: #{loopcount}"
 
-      message = Message.new(:body => message_body, :name => brain_id, :channel_id => "261419F9-67DC-4CA6-A408-A648E09211F0")
+      message = Message.new(:body => message_body, :name => brain_id, :channel_id => "1180F9D2-50E0-4F52-B5C4-355DFDD9E7DE")
       message.save!
 
       sleep(5)
